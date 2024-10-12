b082ef6687ab7f7ba457db375db89c606697e120
Parth Narkar <parthnarkarofficial@gmail.com>
2024-10-12 11:47:24 +0530

    Variables and Constants Completed

---
+++ Basics/variable_constants.js
@@ -0,0 +1,12 @@
+const accountId = 94594;
+let accountEmail = "parth@gmail.com"
+var accountPassword = "23434"
+accountCity = "Jaipur"
+
+// accountId = 943984 - NOT ALLOWED
+
+accountEmail = "dkj@gmail.com"
+accountPassword = 392030
+accountCity = "Bengaluru"
+
+console.table([accountId, accountEmail,accountPassword,accountCity]);
