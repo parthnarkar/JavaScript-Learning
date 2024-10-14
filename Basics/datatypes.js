66132a46f1689173ac72b48d755841181f6d3958
Parth Narkar <parthnarkarofficial@gmail.com>
2024-10-14 20:53:41 +0530

    Datatypes Started

---
+++ Basics/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) - We are using Node JS not browser that is why this is giving an error
+
+//----------------DATA TYPES-----------------------------
+
+let name = "hitesh"
+let age = 20
+let isLoggedIn = false
+let state
+
+//number => 2 to power 53
+//bigint => very big number
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => the value is not yet assigned 
+//symbols => unique (Mostly Used in React)
+
+
+// Objects
+
+console.log(typeof "parth");
+
+console.log(typeof undefined); //undefined has undefined type
+console.log(typeof null); //NULL is OBJECT
