f647f9c1bd44dba08fb1d792a2117a0c0696fd71
Parth Narkar <parthnarkarofficial@gmail.com>
2025-02-04 21:35:48 +0530

    Refactored calculator using closures, bind, and apply; improved structure and fixed operator assignments.

--- simple-calculator/script.js
+++ simple-calculator/script.js
@@ -1,42 +1,44 @@
-function calculate(character){
-    let number1 = document.getElementById('num1').value;
-    let number2 = document.getElementById('num2').value;
-    let result;
-
-    //Convert input values to numbers
-    number1 = parseFloat(number1);
-    number2 = parseFloat(number2);
+//Getting numbers from the user
+function getNum(){
+    let number1 = parseFloat(document.getElementById('num1').value);
+    let number2 = parseFloat(document.getElementById('num2').value);
 
     if(isNaN(number1) || isNaN(number2)){
         document.getElementById('answer').innerHTML = 'Enter Valid Numbers';
-        return;
+        return null;
     }
 
-    switch (character) {
-        case '+':
-            result = number1 + number2;
-            break;
+    return [number1 , number2];
+}
 
-        case '-':
-            result = number1 - number2;
-            break;
-        
-        case '*':
-            result = number1 * number2;
-            break;
-
-        case '/':
-            if(number2 === 0){
-                document.getElementById('answer').innerHTML = 'Cannot Divide by Zero';
-                return;
-            }
-            result = number1 / number2;
-            break;            
-    
-        default:
-            document.getElementById('answer').innerHTML = 'Enter Valid Numbers';
-            break;
+//Adding Closure - Function that returns the function
+function calculateOperation(operator){
+    return function(number1 , number2) {
+        switch(operator){
+            case '+' : return number1 + number2;
+            case '-' : return number1 - number2;
+            case '*' : return number1 * number2;
+            case '/' : return number2 !== 0 ? number1/number2 : 'Cannot Divide by Zero';
+            default : return 'Invalid Operations';
+        }
     }
+}
 
+let add = calculateOperation('+');
+let sub = calculateOperation('-');
+let mul = calculateOperation('*');
+let div = calculateOperation('/');
+
+function calculate(operation){
+    let nums = getNum();
+    if(!nums) return null;
+
+    let result = operation.apply(null , nums);
     document.getElementById('answer').innerHTML = 'RESULT: ' + result;
 }
+
+//First User Clicks on the buttons
+document.getElementById('addBtn').addEventListener('click' , calculate.bind(null , add));
+document.getElementById('subBtn').addEventListener('click' , calculate.bind(null , sub));
+document.getElementById('mulBtn').addEventListener('click' , calculate.bind(null , mul));
+document.getElementById('divBtn').addEventListener('click' , calculate.bind(null , div));
