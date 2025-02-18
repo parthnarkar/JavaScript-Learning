289342d61147bde87e8d9a87d948e27056f00057
Parth Narkar <parthnarkarofficial@gmail.com>
2025-02-18 16:57:43 +0530

    Added localStorage support and improved task management in the to-do list.

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,18 +1,43 @@
-let input = document.getElementById("entered-task");
-let task = document.querySelector(".all-tasks");
+let userInput = document.getElementById('entered-task');
+let addButton = document.getElementById('add-task');
+let allTasks = document.querySelector('.all-tasks');
 
-function addTask(){
-    if(input.value == ""){
-        alert("Enter the Valid Task");
+addButton.addEventListener('click' , () => {
+    let task = userInput.value.trim();
+
+    if(task === ""){
+        alert("Enter Valid Tasks");
     }
     else{
-        let newEle = document.createElement("ul");
-        newEle.innerHTML = `${input.value} <i class="fa fa-trash"></i>`;
-        task.appendChild(newEle);
-        input.value = "";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove();
-        }
+        let tasks = JSON.parse(localStorage.getItem("userTasks")) || [];
+        tasks.push(task);
+        localStorage.setItem("userTasks" , JSON.stringify(tasks));
+
+        let newEle = document.createElement('div');
+        newEle.innerHTML = `${task}    <i class="fa fa-trash"></i>`
+        allTasks.appendChild(newEle);
+
+        userInput.value = "";
     }
+})
+
+window.addEventListener('load' , () => {
+    let tasks = JSON.parse(localStorage.getItem("userTasks")) || [];
+    tasks.forEach(task => {
+        let newEle = document.createElement('div');
+        newEle.innerHTML = `${task}    <i onclick = deleteTask(this) class="fa fa-trash"></i>`;
+        allTasks.appendChild(newEle);
+    });
+})
+
+function deleteTask(element){
+    let delTask = element.parentElement;
+    let taskText = delTask.innerText.trim();
+
+    delTask.remove();
+
+    let tasks = JSON.parse(localStorage.getItem("userTasks")) || [];
+    tasks = tasks.filter(task => task !== taskText);
+    localStorage.setItem("userTasks" , JSON.stringify(tasks));
+
 }
